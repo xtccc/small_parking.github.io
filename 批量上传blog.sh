196a300d6d06fbae2dbaee2f9b191c8691e29f50
xtcc <2927497979@qq.com>
2020-04-13 12:28:44 +0800

    无

--- 批量上传blog.sh
+++ 批量上传blog.sh
@@ -6,13 +6,13 @@ git status
 echo "开始git add ."
 git add .
 
-echo -n "输入commit 描述"
+echo -n "输入commit 描述:"
 read input
 
-echo -n "开始commit"
+echo -n "开始commit:"
 git commit -m $input
 
-echo -n "开始push"
+echo -n "开始push:"
 git push
 
 exit
